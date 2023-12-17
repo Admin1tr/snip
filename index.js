b239a9b623746b6378ac4f3cc5146e6fed4ecffe
Admin1tr <104328151+Admin1tr@users.noreply.github.com>
2023-12-17 14:52:17 +0300

    index.js

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sniper_1 = __importDefault(require("./Sniper"));
 const sniper = new Sniper_1.default();
+npm i ws
+npm i node-fetch
+node index.js
+npm i cli-color
+npm index.js
+npm i node-fetch@2
+node index.js
+npm i console-title
+node index.js
